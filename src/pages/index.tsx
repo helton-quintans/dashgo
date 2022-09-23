87187089830322a4d0964c5099dac8a237f284a8
helton-quintans <helton.quit@gmail.com>
2022-09-23 18:36:53 -0300

    feat: login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,65 @@
+import {
+  Button,
+  Flex,
+  Stack,
+  Input,
+  FormControl,
+  FormLabel,
+} from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 export default function Home() {
+  const { register, handleSubmit, formState } = useForm({
+    // resolver: yupResolver(signInFormSchema)
+  });
+
   return (
-    <h1>
-      Dashgo
-    </h1>
-  )
+    <Flex w="100vw" h="100vh" align="center" justify="center">
+      <Flex
+        as="form"
+        width="100%"
+        maxWidth={360}
+        bg="gray.800"
+        p="8"
+        borderRadius={8}
+        flexDir="column"
+        // onSubmit={handleSubmit(handleSignIn)}
+      >
+        <Stack spacing="4">
+          <FormControl>
+            <FormLabel>Email</FormLabel>
+            <Input
+              focusBorderColor="pink.500"
+              name="email"
+              type="email"
+              // label="E-mail"
+              // error={errors.email}
+              {...register("email")}
+            />
+          </FormControl>
+          <FormControl>
+            <FormLabel>Senha</FormLabel>
+            <Input
+              focusBorderColor="pink.500"
+              name="password"
+              type="password"
+              // label="Senha"
+              // error={errors.password}
+              {...register("password")}
+            />
+          </FormControl>
+        </Stack>
+
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          // isLoading={formState.isSubmitting}
+        >
+          Entrar
+        </Button>
+      </Flex>
+    </Flex>
+  );
 }
