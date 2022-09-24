cfcefc96e35d555483b230f7163099b71bbf873b
helton-quintans <helton.quit@gmail.com>
2022-09-24 14:23:40 -0300

    feat: Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,9 @@
-import { Flex, Text, Input, Icon } from "@chakra-ui/react";
-import { RiSearchLine } from "react-icons/ri"
+import { Flex, Text, Input, Icon, HStack, Box, Avatar} from "@chakra-ui/react";
+import {
+  RiNotificationLine,
+  RiSearchLine,
+  RiUserAddLine,
+} from "react-icons/ri";
 
 export function Header() {
   return (
@@ -27,13 +31,31 @@ export function Header() {
           variant="unstyled"
           color="gray.50"
           placeholder="Buscar na plataforma"
-          _placeholder={{color: "gray.400"}}
+          _placeholder={{ color: "gray.400" }}
           px="4"
           mr="4"
         />
 
-        <Icon as={RiSearchLine} fontSize="20"/>
+        <Icon as={RiSearchLine} fontSize="20" />
+      </Flex>
 
+      <Flex align="center" ml="auto">
+        <HStack  spacing="4" mx="8" pr="8" py="1" color="gray.300" borderColor="gray.700" borderRightWidth={1}>
+          <Icon as={RiNotificationLine} fontSize="20" />
+          <Icon as={RiUserAddLine} fontSize="20" />
+        </HStack>
+
+        <Flex
+          align="center"
+        >
+          <Box mr="4" textAlign="right">
+            <Text>Helton Quintans</Text>
+            <Text color="gray.300" fontSize="s">helton.quit@gmail.com</Text>
+          </Box>
+
+          <Avatar size="md" name="Helton Quintãns" src='https://github.com/helton-quintans.png'/>
+
+        </Flex>
       </Flex>
     </Flex>
   );
