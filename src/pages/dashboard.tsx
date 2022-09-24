11e586cb4e9a3bd72dbb83fe4002fd5c7abe8e41
helton-quintans <helton.quit@gmail.com>
2022-09-24 15:20:30 -0300

    feat: simple grid para receber os gráficos

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,5 +1,5 @@
 import { Header } from "../components/Header";
-import { Flex } from "@chakra-ui/react"
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react"
 import { Sidebar } from "../components/Sidebar";
 
 export default function Dashboard() {
@@ -9,6 +9,32 @@ export default function Dashboard() {
 
             <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
                 <Sidebar />
+
+                <SimpleGrid flex="1" gap="4" minChildWidth="320px" >
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                        // pb="4"
+                    >
+                        <Text fontSize="lg" mb="4" >
+                            Inscritos da semana
+                        </Text>
+                    </Box>
+
+                    <Box
+                        p="8"
+                        bg="gray.800"
+                        borderRadius={8}
+                        // pb="4"
+                    >
+                        <Text fontSize="lg" mb="4" >
+                            Taxa de abertura
+                        </Text>
+
+                    </Box>
+
+                </SimpleGrid>
 
             </Flex>
         </Flex>
