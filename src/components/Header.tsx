367081b430e4b72f8940d006596f216a76c5fb80
helton-quintans <helton.quit@gmail.com>
2022-09-24 12:18:22 -0300

    feat: input de pesquisa

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,40 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text, Input, Icon } from "@chakra-ui/react";
+import { RiSearchLine } from "react-icons/ri"
 
 export function Header() {
   return (
     <Flex as="header" w="100%" maxWidth={1480} h="20" mx="4" px="6" mt="4">
-      <Text fontSize="3xl" letterSpacing="tight">
+      <Text fontSize="3xl" fontWeight="bold" mt="4" letterSpacing="tight">
         dashgo
         <Text as="span" color="pink.500" ml="1">
           .
         </Text>
       </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        position="relative"
+        bgColor="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          variant="unstyled"
+          color="gray.50"
+          placeholder="Buscar na plataforma"
+          _placeholder={{color: "gray.400"}}
+          px="4"
+          mr="4"
+        />
+
+        <Icon as={RiSearchLine} fontSize="20"/>
+
+      </Flex>
     </Flex>
   );
 }
