f5f800f4a3705ce7eaa42276fa4d8b2c7bb397f5
helton-quintans <helton.quit@gmail.com>
2022-09-24 15:42:36 -0300

    feat: Chart sendo carregado pelo browser -> ssr: false

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,43 +1,49 @@
 import { Header } from "../components/Header";
-import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react"
+import { Flex, SimpleGrid, Box, Text } from "@chakra-ui/react";
 import { Sidebar } from "../components/Sidebar";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"), {
+    ssr: false,
+} );
+
+const options = {};
+
+const series = [{ name: "series", data: [31, 65, 10, 120, 1, 33] }];
 
 export default function Dashboard() {
-    return (
-        <Flex direction="column" h="100vh">
-            <Header/>
+  return (
+    <Flex direction="column" h="100vh">
+      <Header />
 
-            <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
-                <Sidebar />
+      <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
+        <Sidebar />
 
-                <SimpleGrid flex="1" gap="4" minChildWidth="320px" >
-                    <Box
-                        p="8"
-                        bg="gray.800"
-                        borderRadius={8}
-                        // pb="4"
-                    >
-                        <Text fontSize="lg" mb="4" >
-                            Inscritos da semana
-                        </Text>
-                    </Box>
+        <SimpleGrid flex="1" gap="4" minChildWidth="320px">
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            // pb="4"
+          >
+            <Text fontSize="lg" mb="4">
+              Inscritos da semana
+            </Text>
+            <Chart options={options} series={series} type="area" height={160} />
+          </Box>
 
-                    <Box
-                        p="8"
-                        bg="gray.800"
-                        borderRadius={8}
-                        // pb="4"
-                    >
-                        <Text fontSize="lg" mb="4" >
-                            Taxa de abertura
-                        </Text>
-
-                    </Box>
-
-                </SimpleGrid>
-
-            </Flex>
-        </Flex>
-
-    )
+          <Box
+            p="8"
+            bg="gray.800"
+            borderRadius={8}
+            // pb="4"
+          >
+            <Text fontSize="lg" mb="4">
+              Taxa de abertura
+            </Text>
+          </Box>
+        </SimpleGrid>
+      </Flex>
+    </Flex>
+  );
 }
