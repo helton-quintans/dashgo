9ce9e250e4271d0de6a7555236575e597b729244
helton-quintans <helton.quit@gmail.com>
2022-09-25 00:11:05 -0300

    styles: Responsividade da Logo

--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -2,7 +2,7 @@ import { Text } from "@chakra-ui/react";
 
 export function Logo() {
     return (
-        <Text fontSize="3xl" fontWeight="bold" mt="4" letterSpacing="tight">
+        <Text fontSize={["2xl", "3xl"]} fontWeight="bold" mt="5" letterSpacing="tight" align="center">
         dashgo
         <Text as="span" color="pink.500" ml="1">
           .
