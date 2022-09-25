6c32f1dd8c473c7656ee7bca1e311df9440a3276
helton-quintans <helton.quit@gmail.com>
2022-09-25 01:18:00 -0300

    feat: Fechar e abrir sidebar através do menu hamburguer

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 import { Logo } from "./Logo";
 import { NotificationsNav } from "./NotificationsNav";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -12,6 +15,18 @@ export function Header() {
 
   return (
     <Flex as="header" w="100%" maxWidth={1480} h="20" mx="4" px="6" mt="4">
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          mr="4"
+          mt="5"
+          onClick={onOpen}
+        ></IconButton>
+      )}
+
       <Logo />
 
       {isWideVersion && <SearchBox />}
--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -13,10 +13,12 @@ import {
   DrawerHeader,
   DrawerBody,
 } from "@chakra-ui/react";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
 
 import { SidebarNav } from "./SidebarNAv";
 
 export function Sidebar() {
+  const { isOpen, onClose } = useSidebarDrawer();
   const isDrawerSidebar = useBreakpointValue({
     base: true,
     lg: false,
@@ -24,19 +26,19 @@ export function Sidebar() {
 
   if (isDrawerSidebar) {
     return (
-      <Drawer isOpen={true} placement="left" onClose={() => {}}>
-      <DrawerOverlay>
-        <DrawerContent bg="gray.800" p="4">
-          <DrawerCloseButton mt="6" />
-          <DrawerHeader>Navegação</DrawerHeader>
+      <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+        <DrawerOverlay>
+          <DrawerContent bg="gray.800" p="4">
+            <DrawerCloseButton mt="6" />
+            <DrawerHeader>Navegação</DrawerHeader>
 
-          <DrawerBody>
-            <SidebarNav />
-          </DrawerBody>
-        </DrawerContent>
-      </DrawerOverlay>
-    </Drawer>
-    )
+            <DrawerBody>
+              <SidebarNav />
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    );
   }
   return (
     <Box as="aside" w="64" mr="8">
