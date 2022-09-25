1f22f4080d80a4be1890464fc1bc669df8476a12
helton-quintans <helton.quit@gmail.com>
2022-09-25 00:48:09 -0300

    feat: Sidebar flutuante (drawer)

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,46 @@
-import { Flex, Box, Stack, Text, Link, Icon } from "@chakra-ui/react";
 import {
-  RiContactsLine,
-  RiDashboardLine,
-  RiGitMergeLine,
-  RiInputMethodLine,
-} from "react-icons/ri";
+  Flex,
+  Drawer,
+  Box,
+  Stack,
+  Text,
+  Link,
+  Icon,
+  useBreakpointValue,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+} from "@chakra-ui/react";
 
 import { SidebarNav } from "./SidebarNAv";
 
 export function Sidebar() {
+  const isDrawerSidebar = useBreakpointValue({
+    base: true,
+    lg: false,
+  });
+
+  if (isDrawerSidebar) {
+    return (
+      <Drawer isOpen={true} placement="left" onClose={() => {}}>
+      <DrawerOverlay>
+        <DrawerContent bg="gray.800" p="4">
+          <DrawerCloseButton mt="6" />
+          <DrawerHeader>Navegação</DrawerHeader>
+
+          <DrawerBody>
+            <SidebarNav />
+          </DrawerBody>
+        </DrawerContent>
+      </DrawerOverlay>
+    </Drawer>
+    )
+  }
   return (
     <Box as="aside" w="64" mr="8">
-      <SidebarNav/>
+      <SidebarNav />
     </Box>
   );
 }
