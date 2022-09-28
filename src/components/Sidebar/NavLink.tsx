382521f08069d5a8a848d5872626f8971ba5d108
helton-quintans <helton.quit@gmail.com>
2022-09-28 13:48:26 -0300

    feat: criação do componente ActiveLink

--- src/components/Sidebar/NavLink.tsx
+++ src/components/Sidebar/NavLink.tsx
@@ -1,6 +1,6 @@
 import { Flex, Box, Stack, Text, Link as ChakraLink, Icon, LinkProps as ChakraLinkProps } from "@chakra-ui/react";
 import { ElementType } from "react";
-import Link from 'next/link'
+import { ActiveLink } from "../ActiveLink";
 
 //!ElementType é passado assim -> ex.: <Icon as={RiDashboardLine} />
 interface NavLinkProps extends ChakraLinkProps{
@@ -11,13 +11,13 @@ interface NavLinkProps extends ChakraLinkProps{
 
 export function NavLink({ icon, children, href, ...rest }: NavLinkProps) {
   return (
-    <Link href={href} passHref>
+    <ActiveLink href={href} passHref>
       <ChakraLink display="flex" {...rest}>
         <Icon as={icon} fontSize="20" />
         <Text ml="4" fontWeight="medium">
           {children}
         </Text>
       </ChakraLink>
-    </Link>
+    </ActiveLink>
   );
 }
