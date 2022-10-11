7926141f2c77ce8c3347c67cd8583b1b79920001
helton-quintans <helton.quit@gmail.com>
2022-10-11 16:49:41 -0300

    chore: config miragejs e instalação da lib react-query

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,9 +1,10 @@
-import { createServer, Factory, Model } from "miragejs"
+import { createServer, Factory, Model } from "miragejs";
+import { faker } from "@faker-js/faker";
 
 type User = {
-  name: string
-  email: string
-  created_at: string
+  name: string;
+  email: string;
+  created_at: string;
 };
 
 // models são como tabelas no banco
@@ -12,32 +13,36 @@ export function makeServer() {
   const server = createServer({
     models: {
       user: Model.extend<Partial<User>>({}),
-    },  
-
-    factories: {
-        user: Factory.extend({
-            name(i: number) {
-                return `User ${i + 1} `
-            },
-            email() {},
-            createdAt() {},
-        })
-
     },
 
-    seeds(server){
+    factories: {
+      user: Factory.extend({
+        name(i: number) {
+          return `User ${i + 1} `;
+        },
+        email() {
+          return faker.internet.email().toLowerCase();
+        },
+        createdAt() {
+          return faker.date.recent(10);
+        },
+      }),
+    },
 
+    // criar dados assim que o servidor for inicializado
+    seeds(server) {
+      server.createList("user", 200);
     },
 
     routes() {
-      this.namespace = "api"
-      this.timing = 750
+      this.namespace = "api";
+      this.timing = 750;
 
-      this.get("/users")
-      this.post("/users")
+      this.get("/users");
+      this.post("/users");
 
-      this.namespace = ""
-      this.passthrough()
+      this.namespace = "";
+      this.passthrough();
     },
   });
 
