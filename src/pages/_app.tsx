31bcbbb06175ddde4b3ce26b1628ab1beaaf1d00
helton-quintans <helton.quit@gmail.com>
2022-10-11 17:16:25 -0300

    feat: config QueryClientProvider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,19 +2,24 @@ import { AppProps } from "next/app";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 
+import { QueryClient, QueryClientProvider } from "react-query";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { makeServer } from "../services/mirage";
 
-if (process.env.NODE_ENV === 'development') {
-  makeServer()
+if (process.env.NODE_ENV === "development") {
+  makeServer();
 }
+
+const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider theme={theme}>
-      <SidebarDrawerProvider>
-        <Component {...pageProps} />
-      </SidebarDrawerProvider>
+      <QueryClientProvider client={queryClient}>
+        <SidebarDrawerProvider>
+          <Component {...pageProps} />
+        </SidebarDrawerProvider>
+      </QueryClientProvider>
     </ChakraProvider>
   );
 }
