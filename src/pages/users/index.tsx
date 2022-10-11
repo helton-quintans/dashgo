6a1cdc5aec66a244955396a028760627c1bd95c1
helton-quintans <helton.quit@gmail.com>
2022-10-11 18:33:24 -0300

    feat: sipinner à direita do heding [usuários] no isFetching

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -25,24 +25,30 @@ import Pagination from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery("users", async () => {
-    const response = await fetch("http://localhost:3000/api/users");
-    const data = await response.json();
+  const { data, isLoading, isFetching, error } = useQuery(
+    "users",
+    async () => {
+      const response = await fetch("http://localhost:3000/api/users");
+      const data = await response.json();
 
-    const users =  data.users.map(user => {
-      return {
-        id: user.id,
-        name: user.name,
-        email: user.email,
-        createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-          day: '2-digit',
-          month: 'long',
-          year: 'numeric'
-        })
-      }
-    })
-    return users
-  });
+      const users = data.users.map((user) => {
+        return {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          createdAt: new Date(user.createdAt).toLocaleDateString("pt-BR", {
+            day: "2-digit",
+            month: "long",
+            year: "numeric",
+          }),
+        };
+      });
+      return users;
+    },
+    {
+      staleTime: 1000 * 5, // 5 seconds
+    }
+  );
 
   const isWideVersion = useBreakpointValue({
     base: false,
@@ -57,7 +63,11 @@ export default function UserList() {
 
         <Box flex="1" borderRadius={8} bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
-            <Heading size="lg">Usuários</Heading>
+            <Heading size="lg">
+              Usuários
+
+              { !isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4"/>}
+            </Heading>
             <Link href="/users/create" passHref>
               <Button
                 as="a"
